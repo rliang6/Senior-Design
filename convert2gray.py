744b65784bcb3263313f893b936ef50b9c54e1ee
rliang6 <37962913+rliang6@users.noreply.github.com>
2018-09-19 18:27:14 -0400

    Update convert2gray.py

--- convert2gray.py
+++ convert2gray.py
@@ -1,12 +1,13 @@
 import imageio
 
-fname = '';
-vid = imageio.get_reader(fname);
-fps = vid.get_meta_data()['fps'];
+def convert2gray(fname):
+    fname = '';
+    vid = imageio.get_reader(fname);
+    fps = vid.get_meta_data()['fps'];
 
-writer = imageio.get_writer(fname, fps=fps);
+    writer = imageio.get_writer(fname, fps=fps);
 
-for image in vid:
-    writer.append_data(image[:,:,1]);
+    for image in vid:
+        writer.append_data(image[:,:,1]);
 
-writer.close()
+    writer.close()
