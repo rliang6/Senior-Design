2c5989bb27d5970c8bf4a7da7bfa7bf885baf464
sz64 <43184828+sz64@users.noreply.github.com>
2018-09-19 00:31:58 -0400

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -3,12 +3,12 @@ import math
 import numpy as np
 from scipy import ndimage
 
-fname = '/home/pi/Desktop/video_1.mp4';
+fname = ''; //Type in the name of the video file that will be analyzed
 vid = imageio.get_reader(fname);
 
-for i, image in enumerate(vid):
-    lp = ndimage.gaussian_filter(image, 3);
-    g_hp = image - lp;
-    g_hpsum = (g_hp.sum() - 255000000)/55000000;
+for i, image in enumerate(vid): //Iterates through the frames of the video
+    lp = ndimage.gaussian_filter(image, 3); //Creates a gaussian low-pass-filtered image
+    g_hp = image - lp; //subtracts the gaussian filtered image with the frame to create a high pass filtered image
+    g_hpsum = (g_hp.sum() - 255000000)/55000000; //uses the image values to estimate the number of cars
     g_hpsum = math.ceil(g_hpsum);
-    print('Mean of edge is %1.1f' % (g_hpsum)); // Need to make this save to a file
+    print('Number of cars is %1.1f' % (g_hpsum)); // Need to make this save to a file
