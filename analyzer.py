0ff9635dffd0b345fe96f05f43d5d1e8902a1d1f
sz64 <43184828+sz64@users.noreply.github.com>
2018-09-19 16:50:31 -0400

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -3,12 +3,30 @@ import math
 import numpy as np
 from scipy import ndimage
 
-fname = ''; //Type in the name of the video file that will be analyzed
+fname = input('Please entire the video location: '); #Type in the name of the video file that will be analyzed
+Car1 = input('What was the max number of cars? '); #For calibration purposes
+Car0 = input('What was the min number of cars? '); #In the future this calibration method would be changed
 vid = imageio.get_reader(fname);
+g_hps = [];
+min = 10000000000000;
+max = 0;
 
-for i, image in enumerate(vid): //Iterates through the frames of the video
-    lp = ndimage.gaussian_filter(image, 3); //Creates a gaussian low-pass-filtered image
-    g_hp = image - lp; //subtracts the gaussian filtered image with the frame to create a high pass filtered image
-    g_hpsum = (g_hp.sum() - 255000000)/55000000; //uses the image values to estimate the number of cars
-    g_hpsum = math.ceil(g_hpsum);
-    print('Number of cars is %1.1f' % (g_hpsum)); // Need to make this save to a file
+for i, image in enumerate(vid): #Iterates through the frames of the video
+    lp = ndimage.gaussian_filter(image, 3); #Creates a gaussian low-pass-filtered image
+    g_hp = image - lp; #subtracts the gaussian filtered image with the frame to create a high pass filtered image
+    g_hps.append(g_hp.sum());
+
+for j in g_hps: #Finds the maximum and minimum edge values to calibrate.
+    if (j < min):
+        min = j;
+    if (j > max):
+        max = j;
+    
+x = [int(Car0), int(Car1)];
+y = [min, max];
+m,b = np.polyfit(x, y, 1); #Finds a linear relationship between number of cars and edges on the screen.
+
+for h in g_hps: #Uses the relationship above to estimate the number of cars in the frame.
+    ncar = (h - b)/m;
+    ncar = math.ceil(ncar);
+    print(ncar); #Change this to save/append the data in a file;
