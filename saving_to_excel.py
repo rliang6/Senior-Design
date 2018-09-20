7ed0a21b5c5e9162d713ef7e1b5202233a87e48e
rliang6 <37962913+rliang6@users.noreply.github.com>
2018-09-19 20:01:30 -0400

    Update saving_to_excel.py

--- saving_to_excel.py
+++ saving_to_excel.py
@@ -1,14 +1,8 @@
-import xlsxwriter
-
-arrayncar=[]
-workbook=xlsxwriter.Workbook('ncarfile.xlsx')
-worksheet=workbook.add_worksheet()
-
+file= open(“testfile.txt”, “w”)#creates as an empty file
+file.write('')
+file = open(“testfile.txt”, “a”) 
 for h in g_hps: #Uses the relationship above to estimate the number of cars in the frame.
     ncar = (h - b)/m;
     ncar = math.ceil(ncar);
-    arrayncar.append(ncar);
-worksheet.write(0,0,'the values')
-for value in range(1,len(arrayncar)):
-    worksheet.write_column(arrayncar[value])
-workbook.close()
+    file.write('%d',ncar)
+file.close()
