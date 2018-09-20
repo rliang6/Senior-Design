7c302bf6b3e5fc4915bbe1b2fea274e6d9d3465f
rliang6 <37962913+rliang6@users.noreply.github.com>
2018-09-20 13:22:42 -0400

    Update recordingthevideo.py

--- recordingthevideo.py
+++ recordingthevideo.py
@@ -3,8 +3,8 @@ from time import sleep
 
 camera=PiCamera()
 
-camera.start_preview()
-camera.start_recording('/home/pi/testingit3.h264')
-sleep(60)
-camera.stop_recording()
-camera.stop_preview()
+camera.start_preview()#'turns on' camera
+camera.start_recording('/home/pi/testingit3.h264') # saves file to directory 
+sleep(60) # records for 60 seconds
+camera.stop_recording()# stops recording
+camera.stop_preview()#'turns off' camera
