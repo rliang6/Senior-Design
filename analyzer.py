72076319e77ea9ccb7628b91815414ad09626014
sz64 <43184828+sz64@users.noreply.github.com>
2018-09-19 20:22:39 -0400

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -5,6 +5,7 @@ from scipy import ndimage
 from convert2gray import convert2gray
 
 fname = input('Please entire the video location: '); #Type in the name of the video file that will be analyzed
+logname = input('Please entire the log location: ');
 Car1 = input('What was the max number of cars? '); #For calibration purposes
 Car0 = input('What was the min number of cars? '); #In the future this calibration method would be changed
 vid = imageio.get_reader(fname);
@@ -29,7 +30,12 @@ x = [int(Car0), int(Car1)];
 y = [min, max];
 m,b = np.polyfit(x, y, 1); #Finds a linear relationship between number of cars and edges on the screen.
 
+file= open(logname, 'w')#creates as an empty file
+file.write('')
+file = open(logname, 'a') 
 for h in g_hps: #Uses the relationship above to estimate the number of cars in the frame.
     ncar = (h - b)/m;
     ncar = math.ceil(ncar);
-    print(ncar); #Change this to save/append the data in a file;
+    file.write(str(ncar));
+    file.write('\n');
+file.close()
