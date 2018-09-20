03b14a701dac0e04c458c22e0d61ad7640d3fc3d
rliang6 <37962913+rliang6@users.noreply.github.com>
2018-09-20 13:19:37 -0400

    Update saving_to_excel.py

--- saving_to_excel.py
+++ saving_to_excel.py
@@ -1,10 +1,9 @@
 import xlsxwriter
 
-arrayncar=[9,2,4,4,5,6,7,56,5,4]# include the array with the actual data in it
-workbook=xlsxwriter.Workbook('ncarfile.xlsx')
-worksheet=workbook.add_worksheet()
+arrayncar=[9,2,4,4,5,6,7,56,5,4]# sample data array
+workbook=xlsxwriter.Workbook('ncarfile.xlsx')#creates excel file name
+worksheet=workbook.add_worksheet()#allows user to input info to excel page
 
-worksheet.write(0,0,'the values')
-for value in range(1,len(arrayncar)):
+for value in range(1,len(arrayncar)): # appends every value vertically to the sheet
     worksheet.write(value,0,arrayncar[value])
 workbook.close() 
